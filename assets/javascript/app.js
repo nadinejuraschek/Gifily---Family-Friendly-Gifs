85cef2fed7cdd8cbd4ede663ea4efc75a3c2ba81
Nadine Ju <nadine.jura@online.de>
2019-10-20 10:07:07 -0700

    added reset.css, linked stylesheets in html, and coded buttons and input field

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,7 @@
+/**********************************
+VARIABLES
+**********************************/
+
 // topics to use on my page
 var topics = [
     "apple",
@@ -6,12 +10,42 @@ var topics = [
     "cherry"
 ];
 
+var APIkey = "99Guer29R3DjsR7GWWt80ErqQpSEt5sh2;
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics[i] + "&api_key=" + APIkey + "&limit=10&rating=g";
+
+/**********************************
+FUNCTIONS
+**********************************/
+
 function createButtons() {
+    // stop buttons from repeatedly showing up
+    $("#topic-btn").empty();
     for (var i = 0; i < topics.length; i++) {
-        console.log(topics[i]);
-        var newButton = $("<button>" + topics[i] + "</button>");
-        $("#topic-buttons").append(newButton);
+        var newBtn = $("<button>" + topics[i] + "</button>");
+        $("#topic-btn").append(newBtn);
     }
 }
 
+/**********************************
+EVENTS
+**********************************/
+
+$("#add-btn").on("click", function (event) {
+    // keeps button from trying to send the input off somewhere
+    // lets user press "enter" to trigger button as well
+    event.preventDefault();
+    // grab user input
+    var addInput = $("#add-btn-input").val();
+    console.log(addInput);
+    // add input to array
+    topics.push(addInput);
+    createButtons();
+})
+
+/**********************************
+GAME CODE
+**********************************/
+// create first buttons
 createButtons();
+
+// display 10 gifs when button is clicked
